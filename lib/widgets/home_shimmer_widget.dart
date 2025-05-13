a27bc2c673c600c338bc062b542e138ecbbc94e8
Qaisar Abbas <qaisarabbas5422@gmail.com>
2025-05-13 16:45:04 +0500

    updated commit

--- lib/widgets/home_shimmer_widget.dart
+++ lib/widgets/home_shimmer_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_plus/constants/colors.dart';
 import 'package:shimmer/shimmer.dart';
 
 class HomeShimmerWidget extends StatelessWidget {
